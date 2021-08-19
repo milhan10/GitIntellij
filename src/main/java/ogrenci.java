233123c9c17ae91e30519a1aadb555191b060d84
hp <ilhantuna10@gmail.com>
2021-08-19 19:44:20 +0200

    versiyon 2: Not girisi yapildi

---
+++ src/main/java/ogrenci.java
@@ -0,0 +1,11 @@
+public class ogrenci {
+    public static void main(String[] args) {
+
+
+        System.out.println("not girisi bölüm cagirildi");
+    }
+
+    public static void OgrenciNotEkle(){
+        System.out.println("not girisi");
+    }
+}
